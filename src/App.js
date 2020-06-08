3ca2d6f93f53c57e869e91b9c5487aca4a827fea
jherson_martelo <jherson.martelo@pragma.com.co>
2020-06-07 20:08:51 -0500

    quitando recursos sin usar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home/Home';
 import SearchInput from './components/molecules/SearchInput';
